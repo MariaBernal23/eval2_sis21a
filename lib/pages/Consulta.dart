c746b82386213bd872475a82c6ba40c5a802bba4
MariaBernal23 <marbernalgamez8@gmail.com>
2023-10-02 13:13:54 -0600

    Metodo consulta

--- lib/pages/Consulta.dart
+++ lib/pages/Consulta.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-//comentario random
 class Consulta extends StatefulWidget {
   @override
   State<Consulta> createState() => _MyHomePageState();
